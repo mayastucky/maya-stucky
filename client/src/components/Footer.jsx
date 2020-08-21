a71eeaecab798ef2a6dc11afba799f691caa2976
Maya Stucky <mayaatstucky@gmail.com>
2020-08-21 09:55:40 -0400

    fixed skills

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -21,21 +21,21 @@ const Footer = () => {
             Get in Touch!
           </h3>
         </Bounce>
-        <div class="text-center py-2">
+        <div className="text-center py-2">
           <a href="tel:316-708-6747" style={styles.icons}>
-            <i class="fas fa-mobile fa-5x"></i>
+            <i className="fas fa-mobile fa-5x"></i>
           </a>
           <a
             href="https://www.linkedin.com/in/maya-stucky-4453b0181/"
             style={styles.icons}
           >
-            <i class="fa fa-linkedin-square fa-5x"></i>
+            <i className="fa fa-linkedin-square fa-5x"></i>
           </a>
           <a href="https://github.com/mayastucky" style={styles.icons}>
-            <i class="fa fa-github fa-5x"> </i>
+            <i className="fa fa-github fa-5x"> </i>
           </a>
           <a href="mailto: mayaatstucky@gmail.com" style={styles.icons}>
-            <i class="fa fa-google fa-5x"> </i>
+            <i className="fa fa-google fa-5x"> </i>
           </a>
         </div>
       </div>
